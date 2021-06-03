7ba5e851c81bfbce5417124934e8ab5466326943
michaeldeistler <michael.deistler@uni-tuebingen.de>
2021-06-03 11:39:05 +0200

    code for demo plot

--- sbi/neural_nets/latent_nets.py
+++ sbi/neural_nets/latent_nets.py
@@ -10,11 +10,15 @@ class MeanNet(nn.Module):
         init_means = torch.linspace(0, 1, dim_psi + 2)[1:-1].unsqueeze(1)
         self._means_of_weights = nn.Parameter(init_means)
 
-        init_std = max(0.5 / dim_psi, 0.1)
+        init_std = max(0.5 / dim_psi, 0.2)
         self._stds_of_weights = nn.Parameter(
             torch.tensor([init_std] * dim_psi).unsqueeze(1)
         )
 
+        init_height = 1.0
+        self._heights_of_weights = nn.Parameter(
+            torch.tensor([init_height] * dim_psi).unsqueeze(1)
+        )
         self.dim_z = dim_z
         self.dim_psi = dim_psi
         self.prior_independent_of_theta = True
@@ -22,7 +26,7 @@ class MeanNet(nn.Module):
     def forward(self, theta, z):
         positions = torch.linspace(0, 1, self.dim_z).unsqueeze(0)
         positions = positions.repeat(self.dim_psi, 1)
-        weights = torch.exp(
+        weights = self._heights_of_weights * torch.exp(
             -((positions - self._means_of_weights) ** 2)
             * 0.5
             / self._stds_of_weights ** 2
@@ -47,12 +51,14 @@ class MeanNetEmbedding(nn.Module):
 
         self.mean_net = MeanNet(dim_z, num_gaussians)
         self.encoding_net = nn.Sequential(
-            nn.BatchNorm1d(num_gaussians),
             nn.Linear(num_gaussians, num_hiddens),
+            nn.BatchNorm1d(num_hiddens),
             nn.ReLU(),
             nn.Linear(num_hiddens, num_hiddens),
+            nn.BatchNorm1d(num_hiddens),
             nn.ReLU(),
             nn.Linear(num_hiddens, num_hiddens),
+            nn.BatchNorm1d(num_hiddens),
             nn.ReLU(),
             nn.Linear(num_hiddens, dim_psi),
         )
@@ -79,20 +85,22 @@ class MeanNetTheta(nn.Module):
             nn.ReLU(),
             nn.Linear(num_hiddens, num_hiddens),
             nn.ReLU(),
-            nn.Linear(num_hiddens, 2 * dim_psi),
+            nn.Linear(num_hiddens, 3 * dim_psi),
         )
+        # (3 * ) because means, stds, heights
 
         self.dim_z = dim_z
         self.dim_psi = dim_psi
         self.prior_independent_of_theta = False
 
     def forward(self, theta, z):
-        means_stds = self.net_to_predict_means_and_stds(theta)
-        means = means_stds[:, :1]
-        stds = torch.exp(means_stds[:, 1:])
+        means_stds_heights = self.net_to_predict_means_and_stds(theta)
+        means = means_stds_heights[:, :1]
+        stds = torch.exp(means_stds_heights[:, 1:2])
+        heights = means_stds_heights[:, 2:]
         positions = torch.linspace(0, 1, self.dim_z).unsqueeze(0)
         positions = positions.repeat(self.dim_psi, 1)
-        weights = torch.exp(-((positions - means) ** 2) * 0.5 / stds ** 2)
+        weights = heights * torch.exp(-((positions - means) ** 2) * 0.5 / stds ** 2)
         repeated_z = z.unsqueeze(1)
         repeated_z = repeated_z.repeat(1, self.dim_psi, 1)
         psi = torch.mean(weights * repeated_z, dim=2)
@@ -119,12 +127,14 @@ class MeanNetEmbeddingTheta(nn.Module):
             num_hiddens=num_hiddens_mean_net,
         )
         self.encoding_net = nn.Sequential(
-            nn.BatchNorm1d(num_gaussians + dim_theta),
-            nn.Linear(num_gaussians, num_hiddens_encoding_net),
+            nn.Linear(num_gaussians + dim_theta, num_hiddens_encoding_net),
+            nn.BatchNorm1d(num_hiddens_encoding_net),
             nn.ReLU(),
             nn.Linear(num_hiddens_encoding_net, num_hiddens_encoding_net),
+            nn.BatchNorm1d(num_hiddens_encoding_net),
             nn.ReLU(),
             nn.Linear(num_hiddens_encoding_net, num_hiddens_encoding_net),
+            nn.BatchNorm1d(num_hiddens_encoding_net),
             nn.ReLU(),
             nn.Linear(num_hiddens_encoding_net, dim_psi),
         )
@@ -138,3 +148,42 @@ class MeanNetEmbeddingTheta(nn.Module):
         psi = self.encoding_net(torch.cat((mean_embedding, theta), dim=1))
 
         return psi
+
+
+class MeanNetDemo(nn.Module):
+    """
+    Used only to make a figure...
+    """
+
+    def __init__(self, dim_z: int, dim_psi: int):
+        super().__init__()
+
+        init_means = torch.linspace(0, 1, dim_psi + 2)[1:-1].unsqueeze(1)
+        self._means_of_weights = nn.Parameter(init_means)
+
+        init_std = max(0.5 / dim_psi, 0.2) / 2.0
+        self._stds_of_weights = nn.Parameter(
+            torch.tensor([init_std] * dim_psi).unsqueeze(1)
+        )
+
+        init_height = 1.0
+        self._heights_of_weights = nn.Parameter(
+            torch.tensor([init_height] * dim_psi).unsqueeze(1)
+        )
+        self.dim_z = dim_z
+        self.dim_psi = dim_psi
+        self.prior_independent_of_theta = True
+
+    def forward(self, theta, z):
+        positions = torch.linspace(0, 1, self.dim_z).unsqueeze(0)
+        positions = positions.repeat(self.dim_psi, 1)
+        weights = self._heights_of_weights * torch.exp(
+            -((positions - self._means_of_weights) ** 2)
+            * 0.5
+            / self._stds_of_weights ** 2
+        )
+        repeated_z = z.unsqueeze(1)
+        repeated_z = repeated_z.repeat(1, self.dim_psi, 1)
+        psi = torch.mean(weights * repeated_z, dim=2)
+
+        return psi, weights
